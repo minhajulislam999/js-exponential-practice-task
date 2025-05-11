65a9027d7a8ba77174e3833877a6d1f64e550bf2
minhajul islam <mm01882390860@gmail.com>
2025-05-11 15:04:00 +0600

    practice with toExponential()

--- script/exponential.js
+++ script/exponential.js
@@ -10,3 +10,32 @@ console.log(a.toExponential(5)); //1.23457e+6
 
 
 console.log((45.789).toExponential(2)); //4.58e+1
+
+{
+    let a = 5000;
+
+    console.log(a.toExponential(2));
+}
+
+{
+    let a = 12000;
+    console.log(typeof a.toExponential(2)); //result: 1.20e+4
+}
+
+{
+    let b = 0.000345;
+    let a= b.toExponential(4); //result: 3.450e-4
+    console.log(a);
+}
+
+{
+    let c = 1.6789;
+    let d = c.toExponential(0);
+
+    console.log(d);
+}
+
+{
+    console.log((500).toExponential(4));
+    console.log((0.95).toExponential(2));
+}
